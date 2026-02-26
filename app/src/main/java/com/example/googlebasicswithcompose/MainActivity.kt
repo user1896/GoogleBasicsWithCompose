633f5e9b1e74ac06b52a03ef374fa234aa479413
user1896 <abde.user15@gmail.com>
2026-02-26 11:23:10 +0100

    styling the text

--- app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.googlebasicswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Android", from = "Developer")
+                    GreetingText(
+                        modifier = Modifier.padding(4.dp),
+                        message = "Happy Android",
+                        from = "Developer"
+                    )
                 }
             }
         }
@@ -37,15 +44,22 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun GreetingText(modifier: Modifier = Modifier, message: String = "Hello", from: String = "Sender") {
-        Column(modifier = modifier) { // instead of arranging our components as a Column, we could use Row or Box.
+        Column( // instead of arranging our components as a Column, we also could use Row or Box.
+            modifier = modifier.padding(8.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
             Text(
                 text = "$message!",
-                fontSize = 30.sp, // "sp" resizes based on the user's preferred text size under phone settings.
+                fontSize = 100.sp, // "sp" resizes based on the user's preferred text size under phone settings.
                 lineHeight = 116.sp,
+                textAlign = TextAlign.Center
             )
             Text(
                 text = from,
-                fontSize = 26.sp
+                fontSize = 26.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
             )
         }
     }
