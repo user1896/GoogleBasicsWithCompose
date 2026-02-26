dd5076986b6ccdbe5a42ee41e909a40f262d3866
user1896 <abde.user15@gmail.com>
2026-02-26 10:31:45 +0100

    fix the spacing

--- app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,44 +22,39 @@ class MainActivity : ComponentActivity() {
     //  onCreate() function is the entry point.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-    // All functions marked with the @Composable annotation can be called from the setContent() function
-    setContent {
+        // All functions marked with the @Composable annotation can be called from the setContent() function
+        setContent {
             GoogleBasicsWithComposeTheme {
-                // The Scaffold container (a composable function) is used
-                // for the Overall page structure/skeleton.
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
                 Surface(
-                    modifier = Modifier.padding(innerPadding),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Android", from = "Developer")
                 }
             }
         }
     }
-}
 
-@Composable
-fun GreetingText(modifier: Modifier = Modifier, message: String = "Hello", from: String = "Sender") {
-    Column(modifier = modifier) { // instead of arranging our components as a Column, we could use Row or Box.
-        Text(
-            text = "$message!",
-            fontSize = 30.sp, // "sp" resizes based on the user's preferred text size under phone settings.
-            lineHeight = 116.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 26.sp
-        )
+    @Composable
+    fun GreetingText(modifier: Modifier = Modifier, message: String = "Hello", from: String = "Sender") {
+        Column(modifier = modifier) { // instead of arranging our components as a Column, we could use Row or Box.
+            Text(
+                text = "$message!",
+                fontSize = 30.sp, // "sp" resizes based on the user's preferred text size under phone settings.
+                lineHeight = 116.sp,
+            )
+            Text(
+                text = from,
+                fontSize = 26.sp
+            )
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GoogleBasicsWithComposeTheme {
-        GreetingText(message = "Happy Android", from = "Developer")
-    }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        GoogleBasicsWithComposeTheme {
+            GreetingText(message = "Happy Android", from = "Developer")
+        }
     }
 }
