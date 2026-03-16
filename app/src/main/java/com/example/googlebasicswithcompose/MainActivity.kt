bcd24569a7a2494ceb0dd170b3ed6f0e2c9efea6
user1896 <abde.user15@gmail.com>
2026-03-16 10:52:32 +0100

    add the dice images

--- app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/googlebasicswithcompose/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -56,6 +58,13 @@ class MainActivity : ComponentActivity() {
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(R.drawable.dice_1),
+                contentDescription = "1"
+            )
+
+            Spacer(modifier = Modifier.height(16.dp)) // adds space between the image and the button
+
             Button(onClick = { /*TODO*/ }) {
                 Text(stringResource(R.string.roll))
             }
