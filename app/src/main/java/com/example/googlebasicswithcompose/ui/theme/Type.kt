c78b72dbbff3dfbaf3bd71ce3270e04abe94f7fa
user1896 <abde.user15@gmail.com>
2026-06-08 23:33:49 +0100

    add custom Typography

--- app/src/main/java/com/example/googlebasicswithcompose/ui/theme/Type.kt
+++ app/src/main/java/com/example/googlebasicswithcompose/ui/theme/Type.kt
@@ -2,12 +2,34 @@ package com.example.googlebasicswithcompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.googlebasicswithcompose.R
+
+// Initialize the fonts I downloaded
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
